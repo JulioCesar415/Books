b3e7272da1d59254569efeb030e2035de3fcf96c
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2022-01-25 20:23:19 -0800

    viewModel error fixed

--- app/src/main/java/com/example/android/books/bookList/BookListViewModel.kt
+++ app/src/main/java/com/example/android/books/bookList/BookListViewModel.kt
@@ -3,12 +3,9 @@ package com.example.android.books.bookList
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.android.books.network.BookProperty
-import com.example.android.books.network.BookResponse
+import androidx.lifecycle.viewModelScope
 import com.example.android.books.network.BooksApi
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.launch
 
 
 class BookListViewModel : ViewModel(){
@@ -25,19 +22,26 @@ class BookListViewModel : ViewModel(){
 
 
 //    method calls Retrofit service and handles returned JSON String
-    private suspend fun getBookProperties(){
-//        call BooksApi which returns a call object
-//        call enqueue on callback to start network request on background thread
-        BooksApi.retrofitService.getProperties().enqueue(object : Callback<List<BookResponse>> {
-            override fun onResponse(call: Call<List<BookResponse>>, response: Response<List<BookResponse>>) {
-                _response.value = "Success: ${response.body()?.size} Book properties retrieved"
+    private fun getBookProperties(){
+////        call BooksApi which returns a call object
+////        call enqueue on callback to start network request on background thread
+//        BooksApi.retrofitService.getProperties().enqueue(object : Callback<List<BookResponse>> {
+//            override fun onResponse(call: Call<List<BookResponse>>, response: Response<List<BookResponse>>) {
+//                _response.value = "Success: ${response.body()?.size} Book properties retrieved"
+//            }
+//
+//            override fun onFailure(call: Call<List<BookResponse>>, t: Throwable) {
+//                _response.value = "Failure: " + t.message
+//            }
+//
+//        })
+        viewModelScope.launch{
+            try {
+                var listResult = BooksApi.retrofitService.getProperties()
+                _response.value = "Success: ${listResult.size} Book properties retrieved"
+            }catch (e: Exception){
+                _response.value = "Failure: ${e.message}"
             }
-
-            override fun onFailure(call: Call<List<BookResponse>>, t: Throwable) {
-                _response.value = "Failure: " + t.message
-            }
-
-        })
-//        _response.value = "Set Book API response here"
+        }
     }
 }
